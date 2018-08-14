471f1d3cb72692ef47d5743e90bfd6e15f9750ca
Alejandro Gonzalez <alejandrogxv@gmail.com>
2018-08-14 13:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens;
 }
- function 
+ function destructivelyPrependKitten(name){
+   kittens.pop(name)
+   return kittens;
+ }
